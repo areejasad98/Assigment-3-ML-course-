54750889ce29044172edd7fedfec25acc6d8cd5d
areejasad98 <54935960+areejasad98@users.noreply.github.com>
2019-12-08 22:12:07 +0500

    Add files via upload

---
+++ simple_linear_regression_assignment.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 21:25:47 2019
+
+@author: Areej
+"""
+
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('aids.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/4, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red',  marker='*')
+plt.plot(X_train, regressor.predict(X_train), color = 'green' , linestyle='dashed', linewidth=2)
+plt.title('Death Rate vs Year  (Training set)')
+plt.xlabel('Death Rate')
+plt.ylabel('Year')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'orange', marker='s')
+plt.plot(X_train, regressor.predict(X_train), color = 'purple', linestyle='dashed', linewidth=2)
+plt.title('Death Rate vs Year (Test set)')
+plt.xlabel('Year')
+plt.ylabel('Death Rate')
+plt.show()
+
+print(regressor.predict([[2000]]))
